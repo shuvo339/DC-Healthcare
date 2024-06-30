e6b83cb636ca1f52f5fe8093b073663aff27bac4
shuvo339 <shubhroshafiullah@gmail.com>
2024-07-01 00:24:23 +0600

    basic project setup

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,20 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Root from "../layouts/Root";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
+
+ export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root></Root>,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+      ],
+    },
+  ]);
